28bc5ccecddeeddd4c4054e02e5b984104dc8888
Leon Schlömmer <leon.schloemmer@posteo.at>
2019-10-06 11:47:13 +0200

    added 2 empty service projects

---
+++ dates/src/test/java/at/leonschloemmer/NativeExampleResourceIT.java
@@ -0,0 +1,9 @@
+package at.leonschloemmer;
+
+import io.quarkus.test.junit.SubstrateTest;
+
+@SubstrateTest
+public class NativeExampleResourceIT extends ExampleResourceTest {
+
+    // Execute the same tests but in native mode.
+}
